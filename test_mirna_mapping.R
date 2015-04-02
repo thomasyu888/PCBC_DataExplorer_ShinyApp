3d09b6e9a433fc7ba8f56619afa1673d8d752cf1
kdaily <kdaily@users.noreply.github.com>
2015-04-01 22:43:35 -0700

    use new mirna to gene mapping

--- test_mirna_mapping.R
+++ test_mirna_mapping.R
@@ -16,41 +16,53 @@ miRNA_to_genes <- read.delim(miRNA_to_genesFile@filePath, header=T, sep="\t",
 miRNA_normCounts_newFile <- synGet('syn3355993')
 # miRNA_normCounts_new <- fread(getFileLocation(miRNA_normCounts_newFile), data.table=FALSE)
 miRNA_normCounts_new <- read.delim(miRNA_normCounts_newFile@filePath, header=T, sep='\t', 
-                                   as.is=T, stringsAsFactors = F, check.names=F)
-rownames(miRNA_normCounts_new) <- tolower(gsub('-.p', '', miRNA_normCounts_new$id))
-miRNA_normCounts_new$id <- NULL
+                                   as.is=T, stringsAsFactors = F, check.names=F) %>%
+  rename(mirName=id)
+# rownames(miRNA_normCounts_new) <- tolower(gsub('-.p', '', miRNA_normCounts_new$id))
+# miRNA_normCounts_new$id <- NULL
 
-miRNA_normCountsFile <- synGet('syn2701942')
-miRNA_normCounts <- read.delim(miRNA_normCountsFile@filePath, header=T, sep='\t', 
-                               as.is=T, stringsAsFactors = F, check.names=F)
+miRNA_to_genesFile_new <- synGet('syn3461627', version=1)
+miRNA_to_genes_new <- read.delim(miRNA_to_genesFile_new@filePath,
+                                 col.names=c("mirName", "GeneID"),
+                                 header=T, sep="\t",
+                                 stringsAsFactors=FALSE, check.names=F)
 
-## Group by the mature and take distinct
-## Some have different precursors, but the values are all identical!
-miRNA_normCounts_unique <- miRNA_normCounts %>%
-  mutate(mirName=str_replace(str_extract(mir, ",.*"), ",", "")) %>% 
-  group_by(mirName) %>% 
-  distinct() %>% 
-  as.data.frame()
-
-rownames(miRNA_normCounts_unique) <- miRNA_normCounts_unique$mirName
-miRNA_normCounts_unique$mir <- NULL
-# miRNA_normCounts_unique$mirMature <- NULL
-
-anti_join(miRNA_normCounts_unique, miRNA_to_genes, by="mirName")
+miRNA_to_genes_new %>%
+  count(mirName) %>%
+  summarize(mean_degree=mean(n))
 
 
-miRNA_to_genes <- merge(temp_miRNAs_names, 
-                        miRNA_to_genes, 
-                        by.x='miRNAPrecursor', by.y='mirName', 
-                        all.x=T)
+# miRNA_normCountsFile <- synGet('syn2701942')
+# miRNA_normCounts <- read.delim(miRNA_normCountsFile@filePath, header=T, sep='\t', 
+#                                as.is=T, stringsAsFactors = F, check.names=F)
+# 
+# ## Group by the mature and take distinct
+# ## Some have different precursors, but the values are all identical!
+# miRNA_normCounts_unique <- miRNA_normCounts %>%
+#   mutate(mirName=str_replace(str_extract(mir, ",.*"), ",", "")) %>% 
+#   group_by(mirName) %>% 
+#   distinct() %>% 
+#   as.data.frame()
 
-#remove dups
-miRNA_to_genes <- miRNA_to_genes[!duplicated(miRNA_to_genes),]
-
-## Convert mir name to precursor
-miRNA_precursors <- data.frame(miRNAPrecursor=rownames(miRNA_normCounts_new)) %>%
-  mutate(miRNAPrecursor=str_replace(miRNAPrecursor, "-[35]p", ""))
-
-miRNA_to_genes %>% filter(mirName %in% miRNA_precursors$miRNAPrecursor) %>% nrow()
-
-miRNA_to_genes %>% filter(mirName %in% temp_miRNAs_names$miRNAPrecursor) %>% nrow()
+# rownames(miRNA_normCounts_unique) <- miRNA_normCounts_unique$mirName
+# miRNA_normCounts_unique$mir <- NULL
+# # miRNA_normCounts_unique$mirMature <- NULL
+# 
+# anti_join(miRNA_normCounts_unique, miRNA_to_genes, by="mirName")
+# 
+# 
+# miRNA_to_genes <- merge(temp_miRNAs_names, 
+#                         miRNA_to_genes, 
+#                         by.x='miRNAPrecursor', by.y='mirName', 
+#                         all.x=T)
+# 
+# #remove dups
+# miRNA_to_genes <- miRNA_to_genes[!duplicated(miRNA_to_genes),]
+# 
+# ## Convert mir name to precursor
+# miRNA_precursors <- data.frame(miRNAPrecursor=rownames(miRNA_normCounts_new)) %>%
+#   mutate(miRNAPrecursor=str_replace(miRNAPrecursor, "-[35]p", ""))
+# 
+# miRNA_to_genes %>% filter(mirName %in% miRNA_precursors$miRNAPrecursor) %>% nrow()
+# 
+# miRNA_to_genes %>% filter(mirName %in% temp_miRNAs_names$miRNAPrecursor) %>% nrow()
